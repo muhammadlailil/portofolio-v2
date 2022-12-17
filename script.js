96c3aa7dbe6195ac3e527ec3fd273e9cca7edd5e
laililmahfud <laililmahvut@gmail.com>
2022-12-17 11:12:59 +0700

    change to html

--- script.js
+++ script.js
@@ -1,4 +1,81 @@
+
+import skils from './data/skil.json' assert {type: 'json'};
+import portofolio from './data/portofolio.json' assert {type: 'json'};
 $(function(){
+    loadSkils()
+    loadPortofolio()
+})
+const modalDetailPortofolio = new bootstrap.Modal(document.getElementById('modalDetailPortofolio'), {
+    keyboard: false
+})
+
+function showDetailPortofolio(id) {
+    const data = portofolio
+    const porto = data.filter(rows => {
+        return rows.id == id
+    })[0]
+    
+    if(porto){
+        $('.project_detail_image img').attr('src',porto.bg)
+        let tools = ``
+        porto.tools.forEach((key)=>{
+            tools += `<li><a href="javascript:;">${key}</a></li>`
+        })
+        const htmlInformation = `<div class="project_name">
+                <h3 class="mb-4">${porto.name}</h3>
+            </div>
+            <p class="project_description">
+                ${porto.description}
+                <br>
+                <b>Role : ${porto.role}</b>
+                <ul>
+                    ${tools}
+                </ul>
+                <b>Year : ${porto.year}</b>
+            </p>`
+        $('#modalDetailPortofolio .project_detail_information').html(htmlInformation)
+        modalDetailPortofolio.toggle()
+    }    
+}
+
+function loadSkils(){
+    let htmls = ``
+    skils.forEach((item)=>{
+        htmls += `<div class="item swiper-slide">
+            <div class="center-item text-center">
+                <div class="image_preview">
+                    <img src="${item.icon}" alt="">
+                </div>
+                <span>${item.name}</span>
+            </div>
+        </div>`;
+    })
+    $('.skils_list_wrappper').html(htmls)
+    loadSwipper()
+}
+
+function loadPortofolio(){
+    let htmls = ``
+    portofolio.forEach((item)=>{
+        htmls += ` <div class="col-sm-4">
+            <div class="portofolio_item">
+                <div class="image">
+                    <img src="${item.bg}" alt="">
+                    <div class="overlay details" data-id="${item.id}">
+                        <a href="javascript:;" class="badge text-white bg-primary">Detail</a>
+                    </div>
+                </div>
+                <p class="project_name">${item.name}</p>
+            </div>
+        </div>`
+    })
+    $('.portofolio_content').html(htmls)
+    $('.portofolio_content .portofolio_item .details').on('click',function(){
+        const id = $(this).data('id')
+        showDetailPortofolio(id)
+    })
+}
+function loadSwipper(){
     new Swiper('.swiperSkils', {
         slidesPerView: 8,
         spaceBetween: 20,
@@ -51,37 +128,4 @@ $(function(){
             // when window width is <= 999px
         }
     });
-})
-const listOfPortofolio = $('#listOfPortofolio').val()
-const modalDetailPortofolio = new bootstrap.Modal(document.getElementById('modalDetailPortofolio'), {
-    keyboard: false
-})
-
-function showDetailPortofolio(id) {
-    const data = JSON.parse(listOfPortofolio)
-    const porto = data.filter(rows => {
-        return rows.id == id
-    })[0]
-    
-    if(porto){
-        $('.project_detail_image img').attr('src',porto.bg)
-        let tools = ``
-        for(let[i,key] of porto.tools.entries()){
-            tools += `<li><a href="javascript:;">${key}</a></li>`
-        }
-        const htmlInformation = `<div class="project_name">
-                <h3 class="mb-4">${porto.name}</h3>
-            </div>
-            <p class="project_description">
-                ${porto.description}
-                <br>
-                <b>Role : ${porto.role}</b>
-                <ul>
-                    ${tools}
-                </ul>
-                <b>Year : ${porto.year}</b>
-            </p>`
-        $('#modalDetailPortofolio .project_detail_information').html(htmlInformation)
-        modalDetailPortofolio.toggle()
-    }    
 }
